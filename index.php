be9c3beda51e794744bee3fe7337dbdd5594397b
TonyHuang <hzx0109@gmail.com>
2025-05-08 17:06:12 -0700

    update home page and icons

--- index.php
+++ index.php
@@ -13,8 +13,10 @@
             <div class="carousel-item active">
                 <div class="home-slider" style="background-image: url('img/home/slider1.jpg');">
                     <div class="slider-content">
-                        <p>THE ART OF TEA</p>
-                        <h1>Taste the Satisfying Freshness of Spring</h1>
+                        <p>PURE COCONUT WATER</p>
+                        <h1>100% Coconut <br>
+                            Nothing More,<br>
+                            Nothing Less</h1>
                     </div>
                 </div>
             </div>
@@ -23,8 +25,8 @@
             <div class="carousel-item">
                 <div class="home-slider" style="background-image: url('img/home/slider2.jpg');">
                     <div class="slider-content">
-                        <p>THE JOY OF BREWING</p>
-                        <h1>Discover Your Favorite Blend Today</h1>
+                        <p>CRAFTED TO DELIGHT</p>
+                        <h1>Handcrafted in Japan for Moments That Matter</h1>
                     </div>
                 </div>
             </div>
@@ -35,65 +37,65 @@
         <div class="row">
             <div class="col-lg-3 col-6 p-0">
                 <div class="highlight-wrap">
-                    <i class="fa-solid fa-truck highlight-icon"></i>
-                    <p>FREE STANDARD DELIVERY</p>
-                    <p>On all orders over $100</p>
+                    <?php include 'img/home/icons/icon_premium.svg'; ?>
+                    <p>Premium Selection</p>
+                    <p>Finest in Local and Global</p>
                 </div>
             </div>
 
             <div class="col-lg-3 col-6 p-0">
                 <div class="highlight-wrap">
-                    <i class="fa-solid fa-truck highlight-icon"></i>
-                    <p>FREE STANDARD DELIVERY</p>
-                    <p>On all orders over $100</p>
+                    <?php include 'img/home/icons/icon_wellness.svg'; ?>
+                    <p>Healthy Lifestyle</p>
+                    <p>Everyday Wellness, Naturally</p>
                 </div>
             </div>
 
             <div class="col-lg-3 col-6 p-0">
                 <div class="highlight-wrap">
-                    <i class="fa-solid fa-truck highlight-icon"></i>
-                    <p>FREE STANDARD DELIVERY</p>
-                    <p>On all orders over $100</p>
+                    <?php include 'img/home/icons/icon_canada.svg'; ?>
+                    <p>Proudly Canadian</p>
+                    <p>Rooted in Local Values</p>
                 </div>
             </div>
 
             <div class="col-lg-3 col-6 p-0">
                 <div class="highlight-wrap">
-                    <i class="fa-solid fa-truck highlight-icon"></i>
-                    <p>FREE STANDARD DELIVERY</p>
-                    <p>On all orders over $100</p>
+                    <?php include 'img/home/icons/icon_wholesome.svg'; ?>
+                    <p>Full of Flavor</p>
+                    <p>Healthy Can Still Be Delicious</p>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="container-fluid mt-4">
+    <div class="container-fluid" style="margin-top: 30px; padding-left: 20px; padding-right: 20px;">
         <div class="row gy-3">
             <div class="col-lg-4 col-md-6">
-                <div class="product-card-1">
-                    <div class="product-card-content">
-                        <p>MATCHA TEA</p>
-                        <h3>Enjoy Healthy Drinks</h3>
+                <div class="product-card product-card-1">
+                    <div class="product-card-content text-white">
+                        <p>Coconut Water</p>
+                        <h3>Pure. <br>Refreshing. <br>Simple.</h3>
                         <button class="button-white">SHOP NOW</button>
                     </div>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6">
-                <div class="product-card-2">
+                <div class="product-card product-card-2">
                     <div class="product-card-content">
-                        <p>MATCHA TEA</p>
-                        <h3>Enjoy Healthy Drinks</h3>
+                        <p>BC Juices</p>
+                        <h3>Local. <br>Pure. <br>Fresh.</h3>
                         <button class="button">SHOP NOW</button>
                     </div>
                 </div>
             </div>
 
             <div class="col-lg-4">
-                <div class="product-card-3">
+                <div class="product-card product-card-3">
                     <div class="product-card-content">
-                        <p>MATCHA TEA</p>
-                        <h3>Enjoy Healthy Drinks</h3>
+                        <p>ArrowTree Snacks</p>
+                        <h3>Handcrafted. <br>Delightful. <br>Elegant.</h3>
                         <button class="button">SHOP NOW</button>
                     </div>
                 </div>
@@ -101,35 +103,36 @@
         </div>
     </div>
 
-    <div class="container-fluid">
-        <div class="row section-padding">
-            <div style="width: 40%;">
+    <div class="container-fluid" style="margin-top: 30px; padding-left: 20px; padding-right: 20px;">
+        <div class="row">
+            <div style="width: 44%;">
                 <div class="masked-image masked-image-1"></div>
             </div>
-            <div style="width: 30%;">
+            <div style="width: 28%;">
                 <div class="masked-image masked-image-2">
                     <div class="masked-image-content">
-                        <h2>Make Your Morning More Than Just Good </h2>
+                        <h2>More Than a Business – A Passion for Healthy Living</h2>
                     </div>
                 </div>
             </div>
-            <div style="width: 30%;">
+            <div style="width: 28%;">
                 <div class="masked-image masked-image-3">
                     <div class="masked-image-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>Founded in 2016, BC Orchard is a Canadian company dedicated to crafting honest, great-tasting products made with clean, simple ingredients. While our core offerings focus on natural hydration and everyday wellness, we also believe in thoughtful indulgence. That’s why we offer handcrafted premium snacks from Japan’s ArrowTree— elegant treats to enjoy or gifts.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="custom-container container">
+    <!-- <div class="custom-container container"> -->
+    <div class="container-fluid" style="margin-top: 30px; padding-left: 20px; padding-right: 20px;">
         <div class="row">
             <div class="col-lg-3 col-6 p-0 border-end">
                 <div class="shop-wrap align-items-start">
-                    <p>SHOP OUR TEA</p>
+                    <p>Our Top Picks</p>
                     <h2>Featured Products</h2>
-                    <p>Would you like to spoil your loved one with something special?</p>
+                    <p>Carefully selected for taste, quality, and purpose—these are our favorite picks for everyday refreshment and joy</p>
                     <button class="button">VIEW MORE PRODUCTS</button>
                 </div>
             </div>
@@ -138,7 +141,10 @@
                 <div class="shop-wrap">
                     <img class="main-img" src="img/home/feature-1.jpg" alt="White Peony">
                     <img class="hover-img" src="img/home/feature-1-hover.jpg" alt="White Peony Hover">
-                    <h4 class="product-name">White Peony</h4>
+                    <div class="product-name">
+                        <p>BC Orchard</p>
+                        <h4>100% Pure Coconut Water</h4>
+                    </div>
                 </div>
             </div>
 
@@ -146,7 +152,10 @@
                 <div class="shop-wrap">
                     <img class="main-img" src="img/home/feature-1.jpg" alt="White Peony">
                     <img class="hover-img" src="img/home/feature-1-hover.jpg" alt="White Peony Hover">
-                    <h4 class="product-name">White Peony</h4>
+                    <div class="product-name">
+                        <p>BC Orchard</p>
+                        <h4>100% Pure BC Grown Juices</h4>
+                    </div>
                 </div>
             </div>
 
@@ -154,28 +163,41 @@
                 <div class="shop-wrap">
                     <img class="main-img" src="img/home/feature-1.jpg" alt="White Peony">
                     <img class="hover-img" src="img/home/feature-1-hover.jpg" alt="White Peony Hover">
-                    <h4 class="product-name">White Peony</h4>
+                    <div class="product-name">
+                        <p>Arrow Tree</p>
+                        <h4>Elegant Treats from Japan</h4>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <section class="home-footer">
+    <section class="home-footer" style="margin-top: 140px;">
         <div class="home-footer-content">
             <p>Client Testimonials</p>
             <h2>This tea is absolutely amazing! I feel immediate uplifting effects after consuming. That I have found on any shelf thus far.</h2>
         </div>
     </section>
 
-    <section class="container-fluid">
-        <div class="row section-padding">
-            <div class="col-lg-6 ps-0">
-                <img src="img/home/contact.jpg" alt="" class="img-fluid">
+    <section class="container-fluid" style="margin-top: 140px;">
+        <div class="row">
+            <div class="col-lg-6 ps-0 d-flex align-items-stretch">
+                <div class="img-container">
+                    <img src="img/home/contact.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="scroll-container">
+                    <div class="scroll-content">
+                        <div class="scroll-text">subscribe & save</div>
+                        <div class="scroll-text">subscribe & save</div>
+                        <div class="scroll-text">subscribe & save</div>
+                    </div>
+                </div>
             </div>
 
             <div class="col-lg-6 pe-0 d-flex flex-column justify-content-center align-items-center text-center">
-                <h2>Contact Us</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>We’re Listening</p>
+                <h2>Get In Touch Now</h2>
+                <p>Have a question, idea, or request? Reach out—our team is ready to support you and explore new opportunities.</p>
                 <button class="button">CONTACT US</button>
             </div>
         </div>
