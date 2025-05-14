d600232ffacca8e0917b9988110d7139912cb3b0
TonyHuang <hzx0109@gmail.com>
2025-05-13 17:06:42 -0700

    add locations page

---
+++ locations.php
@@ -0,0 +1,46 @@
+<?php include 'inc/header.php'; ?>
+
+<body>
+    <div class="banner-container locations-banner bottom-margin">
+        <div class="banner-content">
+            <h1>Where to find us</h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <button class="button">ALL</button>
+        <button class="button">Coconut Water</button>
+        <button class="button">Juices</button>
+        <button class="button">Arrow Tree</button>
+
+        <div class="row bottom-margin mt-3">
+            <div class="col-lg-4">
+                <div class="location-card">
+                    <div>
+                        <h4>Brandon & Joanny’s NoFrills</h4>
+                        <div>101-1030 Denman St, Vancouver</div>
+                    </div>
+                    <div>
+                        <div class="devider"></div>
+                        <a href="https://maps.app.goo.gl/jF4piCsshDbHxJWbA" target="_blank" rel="noopener noreferrer">Get Directions</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="location-card">
+                    <div>
+                        <h4>Big Crazy Supermarket (柏仁超市)</h4>
+                        <div>180-4551 No 3 Rd, Richmond</div>
+                    </div>
+                    <div>
+                        <div class="devider"></div>
+                        <a href="https://maps.app.goo.gl/14wo4H5Ck9YUuiJ9A" target="_blank" rel="noopener noreferrer">Get Directions</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<?php include 'inc/footer.php'; ?>
