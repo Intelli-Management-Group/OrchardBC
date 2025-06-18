0791c98ae565607cda00eff52587246ed4e1554b
TonyHuang <hzx0109@gmail.com>
2025-06-18 14:42:21 -0700

    add animation on scroll to about page

--- about.php
+++ about.php
@@ -1,13 +1,14 @@
 <?php include 'inc/header.php'; ?>
 
 <body>
+    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
     <div class="banner-container about-banner">
         <div class="banner-content">
             <h1 class="text-white">About Us</h1>
         </div>
     </div>
 
-    <div class="container">
+    <div class="container overflow-x-hidden">
         <div class="row section-padding">
             <div class="col-lg-8 mx-auto">
                 <p class="text-center">At BC Orchard, we don’t just make food—we make choices that reflect our belief in better living. From our sourcing to our packaging, everything is guided by one promise: to support your well-being with honesty, intention, and care. What you taste is only part of the story. The rest is what we stand for.</p>
@@ -15,7 +16,7 @@
         </div>
 
         <div class="row bottom-margin gy-5 gy-lg-0">
-            <div class="col-lg-6 text-center d-flex flex-column justify-content-center align-items-center border-end-lg">
+            <div class="col-lg-6 text-center d-flex flex-column justify-content-center align-items-center border-end-lg" data-aos="fade-right">
                 <div class="max-width">
                     <div class="upper-header">Our Story</div>
                     <h2 class="mb-4">Made Healthy Living Simple</h2>
@@ -24,7 +25,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-6 px-4 d-flex justify-content-center">
+            <div class="col-lg-6 px-4 d-flex justify-content-center" data-aos="fade-left">
                 <img src="img/about/img_1.jpg" alt="" class="img-fluid">
             </div>
         </div>
@@ -40,28 +41,28 @@
             </div>
 
             <div class="row gy-5 gy-lg-0">
-                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end">
+                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end" data-aos="fade-up" data-aos-delay="0">
                     <img src="img/about/about-image-4.jpg" alt="" class="img-fluid">
                     <div class="upper-header" style="margin-top: 40px;">Excellence</div>
                     <h5>Only the Finest</h5>
                     <p>From sourcing to shelf, quality is non-negotiable. We select only the best ingredients from BC and the global. </p>
                 </div>
 
-                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end-lg">
+                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end-lg" data-aos="fade-up" data-aos-delay="100">
                     <img src="img/about/about-image-5.jpg" alt="" class="img-fluid">
                     <div class="upper-header" style="margin-top: 40px;">Passion</div>
                     <h5>Shared Well-Being</h5>
                     <p>We care deeply about your health – and ours. Our team prioritizes self-care and healthy working environment</p>
                 </div>
 
-                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end">
+                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end" data-aos="fade-up" data-aos-delay="200">
                     <img src="img/about/about-image-6.jpg" alt="" class="img-fluid">
                     <div class="upper-header" style="margin-top: 40px;">Communication</div>
                     <h5>Your Voice Matters</h5>
                     <p>We listen to you – and to each other. Customer feedback shapes new products, while employee ideas drive innovation. </p>
                 </div>
 
-                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end-lg">
+                <div class="col-lg-3 col-md-6 d-flex flex-column align-items-center text-center border-end-lg" data-aos="fade-up" data-aos-delay="300">
                     <img src="img/about/about-image-7.jpg" alt="" class="img-fluid">
                     <div class="upper-header" style="margin-top: 40px;">Respect</div>
                     <h5>For People and Planet</h5>
@@ -71,13 +72,13 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container overflow-x-hidden">
         <div class="row bottom-margin gy-5 gy-lg-0">
-            <div class="col-lg-6 px-4 border-end-lg d-flex justify-content-center">
+            <div class="col-lg-6 px-4 border-end-lg d-flex justify-content-center" data-aos="fade-right">
                 <img src="img/about/about-image-3.jpg" alt="" class="img-fluid">
             </div>
 
-            <div class="col-lg-6 text-center d-flex flex-column justify-content-center align-items-center">
+            <div class="col-lg-6 text-center d-flex flex-column justify-content-center align-items-center" data-aos="fade-left">
                 <div class="max-width">
                     <div class="upper-header">Why It Matters</div>
                     <h2 class="mb-4">Globally Sourced. Locally Rooted.</h2>
@@ -87,6 +88,15 @@
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            duration: 500,
+            easing: 'ease-in-out',
+            once: false
+        });
+    </script>
 </body>
 
 <?php include 'inc/footer.php'; ?>
